f7042b32d3559491203af3ca4080ab813d2355b0
Miguel Luiz <122481365+miguelloq@users.noreply.github.com>
2024-10-29 00:21:30 -0300

    Arvore algebra

--- lib/models/algebra_relacional.dart
+++ lib/models/algebra_relacional.dart
@@ -2,14 +2,34 @@ typedef Tabela = String;
 typedef Expressao = String;
 typedef AtributoTabela = String;
 
-sealed class AlgebraComponente{}
+sealed class AlgebraArvore{}
 
-
-class ProdutoCartesiano implements{
-
+final class AlgebraArvoreSemJoin implements AlgebraArvore{
+   final Projecao projecao;
+  AlgebraArvoreSemJoin({required this.projecao});
 }
 
-class Projecao implements AlgebraComponente{//π
+final class AlgebraArvoreComJoin implements AlgebraArvore{
+   final String raiz;
+   final ProdutoCartesiano produtoCartesiano;
+  AlgebraArvoreComJoin({required this.raiz, required this.produtoCartesiano});
+}
+
+sealed class AlgebraComponente{}
+
+final class NoVazio implements AlgebraComponente{}
+
+final class ProdutoCartesiano implements AlgebraComponente{
+   AlgebraComponente noEsquerdo;
+   AlgebraComponente noDireito;
+  ProdutoCartesiano({required this.noEsquerdo, required this.noDireito});
+
+  void addNoDireito(AlgebraComponente no) => noDireito = no;
+  void addNoEsquerdo(AlgebraComponente no) => noEsquerdo = no;
+  bool temEspaco() => noEsquerdo is NoVazio || noDireito is NoVazio;
+}
+
+final class Projecao implements AlgebraComponente{//π
    final Tabela tabela;
    final List<AtributoTabela> tabelaAtributos;
    final List<Expressao> selecoes; //σ
